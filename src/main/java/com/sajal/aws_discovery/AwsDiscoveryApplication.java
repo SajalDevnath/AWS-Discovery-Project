3d03310f6be5afb78b0fab294f7fc3f55d2aca1f
SajalDevnath <sajaldevnath96@gmail.com>
2024-06-14 22:21:27 +0530

    Initial Commit

---
+++ src/main/java/com/sajal/aws_discovery/AwsDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.sajal.aws_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDiscoveryApplication.class, args);
+		System.out.println("Hello world");
+	}
+}
